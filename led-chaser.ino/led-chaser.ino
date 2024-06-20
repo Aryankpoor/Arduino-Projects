6c24bfb5cc22719976ba9e225c7a9ca792fe5aa0
Aryan Kapoor <64773763+Aryankpoor@users.noreply.github.com>
2024-06-21 01:24:22 +0530

    Add files via upload

---
+++ led-chaser.ino/led-chaser.ino
@@ -0,0 +1,17 @@
+int i;
+int led[]={2,3,4,5,6,7,8,9,10,11,12,13};
+
+void setup() {
+ for(i=0;i<12;i++)
+ {pinMode(led[i],OUTPUT);}
+}
+void loop() {
+  for(i=0;i<12;i++)
+  {digitalWrite(led[i],HIGH);
+  delay(50);
+  digitalWrite(led[i],LOW);}
+  for(i=11;i>=0;i--)
+  {digitalWrite(led[i],HIGH);
+  delay(50);
+  digitalWrite(led[i],LOW);
+}}
